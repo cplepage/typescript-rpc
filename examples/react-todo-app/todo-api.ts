2163faa88280a0dbc4295252d462654e73e1f6f5
cplepage <lepage.charlesphilippe@gmail.com>
2023-01-24 15:02:38 -0500

    array for map, cleaner

--- examples/react-todo-app/todo-api.ts
+++ examples/react-todo-app/todo-api.ts
@@ -6,33 +6,38 @@ export type Todo = {
   done: boolean;
 };
 
-let todos: Todo[] = [];
+const todos: Map<Todo['id'], Todo> = new Map();
 
 export default class Api {
 
   async List() {
-    return todos.map((todo) => todo.id);
+    return Array.from(todos.keys());
   }
 
   @Post()
   async Create(todo: Omit<Todo, 'id'>) {
-    const id = Math.floor(Math.random() * 888889 + 111111);
-    todos.push({ id: id, ...todo });
+    const id = randomIntFromInterval(111111, 999999);
+    todos.set(id, { id: id, ...todo });
     return id;
   }
 
   @Get()
   async Read(id: number) {
-    return todos.find((todo) => todo.id === id);
+    return todos.get(id);
   }
 
   @Put()
-  async Update(id: number, updatedTodo: Todo) {
-    todos = todos.map((todo) => (todo.id === id ? updatedTodo : todo));
+  async Update(id: number, todo: Todo) {
+    todos.set(id, todo);
   }
 
   @Delete()
   async Delete(id: number) {
-    todos = todos.filter((todo) => todo.id !== id);
+    todos.delete(id);
   }
+}
+
+// source: https://stackoverflow.com/a/7228322
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
